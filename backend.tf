302b05bc7dc5ff577199bfbe0161934c380cd475
deepthi7143 <150417779+deepthi7143@users.noreply.github.com>
2024-01-21 12:44:45 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "deepthi777"
     key = "terraform.tfstate"
     region = "us-east-1"
-    dynamodb_table = "deepthidb"
-    -lock = false
+    dynamodb_table = "DynamoDbLock"
+    
   }
 }
